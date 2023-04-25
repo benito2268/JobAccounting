72416120ade3e2b76db4be65741e148ddfacf6b2
Jason Patton <jpatton@cs.wisc.edu>
2023-04-25 15:56:07 -0500

    Fix missing column name for removed filters

--- accounting/filters/ChtcScheddCpuRemovedFilter.py
+++ accounting/filters/ChtcScheddCpuRemovedFilter.py
@@ -369,7 +369,7 @@ class ChtcScheddCpuRemovedFilter(BaseFilter):
         else:
             row["Exec Atts / Shadw Start"] = 0
         if sum(data["_NumBadJobStarts"]) > 0:
-            row["CPU Hours / Bad Exec Att"] = (sum(self.clean(badput_cpu_time)) / 3600) / sum(data["_NumBadJobStarts"])
+            row["CPU Hours / Exec Att"] = (sum(self.clean(badput_cpu_time)) / 3600) / sum(data["_NumBadJobStarts"])
         else:
             row["CPU Hours / Exec Att"] = 0
 
