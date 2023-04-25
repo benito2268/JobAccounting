87c3c29153e80904b9720613ba0c61229ccceee6
Jason Patton <jpatton@cs.wisc.edu>
2023-04-25 09:48:45 -0500

    Missed one file

--- accounting/filters/ChtcScheddCpuFilter.py
+++ accounting/filters/ChtcScheddCpuFilter.py
@@ -481,7 +481,7 @@ class ChtcScheddCpuFilter(BaseFilter):
         else:
             row["Exec Atts / Shadw Start"] = 0
         if sum(data["_NumBadJobStarts"]) > 0:
-            row["CPU Hours / Bad Exec Att"] = (sum(badput_cpu_time) / 3600) / sum(data["_NumBadJobStarts"])
+            row["CPU Hours / Bad Exec Att"] = (sum(self.clean(badput_cpu_time)) / 3600) / sum(data["_NumBadJobStarts"])
         else:
             row["CPU Hours / Bad Exec Att"] = 0
 
