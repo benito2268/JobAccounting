125e522113b90bd82157f6a768793e9935cec141
William Swanson <wnswanson@wisc.edu>
2024-06-05 16:22:24 -0500

    CHTC CPU removed job report homogenization

--- accounting/formatters/ChtcScheddCpuRemovedFormatter.py
+++ accounting/formatters/ChtcScheddCpuRemovedFormatter.py
@@ -69,26 +69,31 @@ class ChtcScheddCpuRemovedFormatter(BaseFormatter):
             "CPU Hours / Exec Att":     lambda x: f"<td>{float(x):.1f}</td>",
             "Shadw Starts / Job Id":    lambda x: f"<td>{float(x):.2f}</td>",
             "Exec Atts / Shadw Start":  lambda x: f"<td>{float(x):.3f}</td>",
+            "Holds / Job Id":           lambda x: f"<td>{float(x):.2f}</td>",
             "% Jobs w/o Shadw":         lambda x: f"<td>{float(x):.1f}</td>",
+            "% Jobs w/1+ Holds":        lambda x: f"<td>{float(x):.1f}</td>",
         }
         rows = super().format_rows(header, rows, custom_fmts=custom_fmts, default_text_fmt=default_text_fmt, default_numeric_fmt=default_numeric_fmt)
         return rows
 
     def get_legend(self):
         custom_items = OrderedDict()
-        custom_items["All CPU Hours"]    = "Total CPU hours for all execution attempts, including preemption and removal"
         custom_items["Num Uniq Job Ids"] = "Number of unique job ids across all execution attempts"
+        custom_items["All CPU Hours"]    = "Total CPU hours for all execution attempts, including preemption and removal"
         custom_items["% Good CPU Hours"] = "Good CPU Hours per All CPU Hours, as a percentage"
         custom_items["Good CPU Hours"]   = "Total CPU hours for execution attempts that ran to completion"
         custom_items["Max Rqst Mem MB"]  = "Maximum memory requested across all submitted jobs in MB"
         custom_items["Max Used Mem MB"]  = "Maximum measured memory usage across all submitted jobs' last execution attempts in MB"
         custom_items["Max Rqst Cpus"]    = "Maximum number of CPUs requested across all submitted jobs"
-
+        
         custom_items["CPU Hours / Exec Att"] = "Average CPU Hours used in a non-final execution attempt"
         custom_items["Rm'd Jobs w/o Shadw Start"] = "Number of jobs that were removed from the queue before any shadow starts"
         custom_items["% Jobs w/o Shadw"] = "Percentage of removed jobs that never had a shadow start, i.e. were removed before running"
+        custom_items["% Jobs w/1+ Holds"] = "Percent of Num Uniq Job Ids that had one or more jobs go on hold"
+
         custom_items["Shadw Starts / Job Id"] = "Num Shadw Starts per (Num Uniq Job Ids - Rm'd Jobs w/o Shadw Start), i.e. per job that had at least one shadow"
         custom_items["Exec Atts / Shadw Start"] = "Num Exec Atts per Num Shadw Starts"
+        custom_items["Holds / Job Id"] = "Num Job Holds per Num Uniq Job Ids"
         custom_items["Avg MB Sent"] = "Mean MB sent to a job sandbox from a submit point"
         custom_items["Avg MB Recv"] = "Mean MB sent to a submit point from a job sandbox"
         custom_items["Num Short Jobs"]   = "Number of execution attempts that completed in less than 60 seconds"
